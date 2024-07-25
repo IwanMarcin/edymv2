1fdb9990a20b5398d2558ae5bc2265662ae5640b
Marcin <marcinm.iwan@gmail.com>
2024-07-25 12:04:13 +0200

    fix of month display

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("generuj").onclick = function(){
+document.getElementById("generuj").onclick = function () {
     const name = document.getElementById("imie").value;
     const fiskalny = document.getElementById("raportfiskalny").value;
     const subiekt = document.getElementById("raportsubiekt").value;
@@ -9,7 +9,7 @@ document.getElementById("generuj").onclick = function(){
     const symbol = document.getElementById("symbolsklepu").value;
 
     let currentDate = new Date();
-    let month = currentDate.getMonth() + 1;
+    let month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
     let year = currentDate.getFullYear();
 
     let x500 = document.getElementById("pln500").value;
@@ -29,18 +29,26 @@ document.getElementById("generuj").onclick = function(){
     let y2 = document.getElementById("gr2").value;
     let y1 = document.getElementById("gr1").value;
 
-
-    let zlotowki = (x500*500)+(x200*200)+(x100*100)+(x50*50)+(x20*20)+(x10*10)+(x5*5)+(x2*2)+(x1*1);
-    let grosze = (y50*0.50)+(y20*0.20)+(y10*0.10)+(y5*0.05)+(y2*0.02)+(y1*0.01);
+    let zlotowki =
+        x500 * 500 +
+        x200 * 200 +
+        x100 * 100 +
+        x50 * 50 +
+        x20 * 20 +
+        x10 * 10 +
+        x5 * 5 +
+        x2 * 2 +
+        x1 * 1;
+    let grosze =
+        y50 * 0.5 + y20 * 0.2 + y10 * 0.1 + y5 * 0.05 + y2 * 0.02 + y1 * 0.01;
     let sumakasa = zlotowki + grosze;
     sumakasa = sumakasa.toFixed(2);
 
     let roz = sumakasa - gotowa;
     roz = roz.toFixed(2);
 
-    if (termi === subkarty){
-document.getElementById("endmail").innerHTML = 
-`Cześć,
+    if (termi === subkarty) {
+        document.getElementById("endmail").innerHTML = `Cześć,
 
 ${fiskalny} zł - Raport fiskalny
 ${subiekt} zł - Raport z Subiekta
@@ -54,12 +62,11 @@ ${sumakasa} zł - Gotówka Kasa
 ${roz} zł - Różnica
 
 Pozdrawiam,
-${name} :)`
-} else if (termi > subkarty) {
-    let roznicakarty = termi - subkarty;
-    roznicakarty = roznicakarty.toFixed(2);
-  document.getElementById("endmail").innerHTML = 
-`Cześć,
+${name} :)`;
+    } else if (termi > subkarty) {
+        let roznicakarty = termi - subkarty;
+        roznicakarty = roznicakarty.toFixed(2);
+        document.getElementById("endmail").innerHTML = `Cześć,
 
 ${fiskalny} zł - Raport fiskalny
 ${subiekt} zł - Raport z Subiekta
@@ -78,13 +85,11 @@ Proszę o poprawę paragonu z PŁATNOŚĆ GOTÓWKA na PŁATNOŚĆ KARTĄ.
 Numer paragonu: PA *popraw*/${symbol}/${month}/${year}
 
 Pozdrawiam,
-${name} :)`  
-
-} else {
-    let roznicakarty = subkarty - termi;
-    roznicakarty = roznicakarty.toFixed(2);
-    document.getElementById("endmail").innerHTML = 
-`Cześć,
+${name} :)`;
+    } else {
+        let roznicakarty = subkarty - termi;
+        roznicakarty = roznicakarty.toFixed(2);
+        document.getElementById("endmail").innerHTML = `Cześć,
 
 ${fiskalny} zł - Raport fiskalny
 ${subiekt} zł - Raport z Subiekta
@@ -103,28 +108,24 @@ Proszę o poprawę paragonu z PŁATNOŚĆ KARTĄ na PŁATNOŚĆ GOTÓWKĄ.
 Numer paragonu: PA *popraw*/${symbol}/${month}/${year}
 
 Pozdrawiam,
-${name} :)`  
-}
+${name} :)`;
     }
+};
 
-    
-
-document.getElementById("wplata").onclick = function(){
-   
+document.getElementById("wplata").onclick = function () {
     let currentDate = new Date();
     let day = currentDate.getDate();
-    let month = currentDate.getMonth() + 1;
+    let month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
     let year = currentDate.getFullYear();
-    let dzisiaj = `${day}.${month}.${year}`
+    let dzisiaj = `${day}.${month}.${year}`;
     const name = document.getElementById("imie").value;
     const punkt1 = document.getElementById("zast").value;
     const punkt2 = document.getElementById("przyp").value;
 
-document.getElementById("endmail").innerHTML =
-`Cześć,
+    document.getElementById("endmail").innerHTML = `Cześć,
 
 W dniu ${dzisiaj} utarg z punktu ${punkt1} został wpłacony na kartę przypisaną do punktu ${punkt2}.
 
 Pozdrawiam,
-${name}`
-}
+${name}`;
+};
